90b7303f7489a14790bfdf6d9d327c78007e6186
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 21:53:53 +0100

    Initial commit

---
+++ src/main/java/simplesumo/simplesumo/SimpleSumo.java
@@ -0,0 +1,17 @@
+package simplesumo.simplesumo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleSumo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
